82576aadbab43ff260e7f06e6d0c9c3fd8168d35
Jordi Clapera <jclapera2@gmail.com>
2024-11-18 16:16:44 +0100

    add wayabr and nvim configurations
    added the configurations to the public repository

---
+++ nvim/init.lua
@@ -0,0 +1,15 @@
+-- Absolute line numbers when using nvim
+vim.o.number = true
+
+-- Wrapping
+vim.o.wrap = true
+
+-- Indentation settings
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+
+vim.cmd('syntax enable')
+
+-- Colorscheme, please change it as you like
+vim.cmd('colorscheme desert')
